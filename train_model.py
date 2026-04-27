c80f68fd305cbe67b41db7697742dcc870aa53c5
sunilakiran <sunilakiran192@gmail.com>
2026-04-27 23:20:20 +0500

    fix(train): update dataset path

--- train_model.py
+++ train_model.py
@@ -6,8 +6,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
-df = load_and_clean("data/KaggleV2-May-2016.csv")
+df = load_and_clean("KaggleV2-May-2016.csv")
 X, y = get_features_and_target(df)
 metrics = train(X, y)
 print("Training complete!")
